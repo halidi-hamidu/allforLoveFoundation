8fa779e6d80c3f8bd648b606ce020cc0d5dadc0d
Halidi <60255981+halidi-hamidu@users.noreply.github.com>
2024-03-13 13:59:29 +0300

    url endpoint for donation added in charityApp/urls.py

--- charityApp/urls.py
+++ charityApp/urls.py
@@ -9,6 +9,9 @@ from .views import BranchListView
 from .views import BranchAPIView
 from .views import SubscribersListView
 from .views import SubscribersAPIView
+from .views import DonationListAPIView
+from .views import DonationAPIView
+
 
 
 app_name = 'charityApp'
@@ -22,6 +25,8 @@ urlpatterns = [
     path('branch/<uuid:pk>/', BranchAPIView.as_view(), name = 'branch'),
     path('subscribers/', SubscribersListView.as_view(), name = 'subsriber'),
     path('subscriber/<uuid:pk>/', SubscribersAPIView.as_view(), name = 'subsriber'),
+    path('donations/', DonationListAPIView.as_view(), name = 'donations'),
+    path('donation/<uuid:pk>/', DonationAPIView.as_view(), name = 'donation'),
     
 ]
     
